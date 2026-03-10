d338f6412e26c8b8fa461000bcf5104c916697fe
Angelovesnow21 <jessica.elaine8468@gmail.com>
2026-03-10 20:49:36 +0600

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,11 @@
+#Printing a greeting message
+print("Hello World!")
+
+#Printing my name
+print("My name is Jessica")
+
+#Printing my hobby
+print("I like to read story books")
+
+# Performing and printing a simple calculation
+print("The result of 2 + 9 is:", 2 + 9)
